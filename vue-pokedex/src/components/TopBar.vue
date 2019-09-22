263fd2a600e985e917cc82a711398dcea1389ed5
Kuinox <nvandeginste@gmail.com>
2019-09-22 12:39:26 +0200

    Component list work on Vue.

--- vue-pokedex/src/components/TopBar.vue
+++ vue-pokedex/src/components/TopBar.vue
@@ -1,21 +1,41 @@
 <template>
-    <el-header class="header">
-        <el-menu>
-            
-        </el-menu>
-    </el-header>
+    <header class="topBar center lightPadding strongBackground">
+        Kuinox's pokedex
+        <form>
+            <input type="text" placeholder="Search..." @input="searchCallback($event.target.value)" />
+        </form>
+    </header>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
-import ElementUI from "element-ui";
 
 @Component
-export default class TopBar extends Vue {}
+export default class TopBar extends Vue {
+    @Prop({required: true})
+    public readonly searchCallback!: (searchText: any) => void;
+}
 </script>
 <style lang="scss">
-@import "@/element-variables.scss";
-.header {
-    background-color: $--color-primary;
+.topBar {
+    position: sticky;
+    position: -webkit-sticky;
+    top: 0;
+    max-height: 10vh;
+    min-height: 4vh;
+    height: 35px;
+    display: flex;
+}
+
+.strongBackground {
+    background-color: #9083a3;
+}
+
+.center {
+    align-items: center;
+}
+
+.lightPadding {
+    padding: 5px 5px 5px 5px;
 }
 </style>
--- vue-pokedex/src/main.ts
+++ vue-pokedex/src/main.ts
@@ -1,11 +1,12 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import './plugins/element.js';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
 Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount('#app');
+import { initializePokemonStore } from "kuinox-pokedex-backend";
+initializePokemonStore("").then(() => {
+  new Vue({
+    router,
+    render: (h) => h(App),
+  }).$mount("#app");
+});
